2f37671fca7c28e8cf9c2f52cd0151fda9f8d099
kelvinfok <punkd1@gmail.com>
2022-12-27 15:24:37 +0800

    lesson 27 - add more tests

--- tip-calculatorTests/tip_calculatorTests.swift
+++ tip-calculatorTests/tip_calculatorTests.swift
@@ -47,6 +47,67 @@ final class tip_calculatorTests: XCTestCase {
     }.store(in: &cancellables)
   }
   
+  func testResultWithoutTipFor2Person() {
+    // given
+    let bill: Double = 100.0
+    let tip: Tip = .none
+    let split: Int = 2
+    let input = buildInput(
+      bill: bill,
+      tip: tip,
+      split: split)
+    // when
+    let output = sut.transform(input: input)
+    // then
+    output.updateViewPublisher.sink { result in
+      XCTAssertEqual(result.amountPerPerson, 50)
+      XCTAssertEqual(result.totalBill, 100)
+      XCTAssertEqual(result.totalTip, 0)
+    }.store(in: &cancellables)
+  }
+  
+  func testResultWith10PercentTipFor2Person() {
+    // given
+    let bill: Double = 100.0
+    let tip: Tip = .tenPercent
+    let split: Int = 2
+    let input = buildInput(
+      bill: bill,
+      tip: tip,
+      split: split)
+    // when
+    let output = sut.transform(input: input)
+    // then
+    output.updateViewPublisher.sink { result in
+      XCTAssertEqual(result.amountPerPerson, 55)
+      XCTAssertEqual(result.totalBill, 110)
+      XCTAssertEqual(result.totalTip, 10)
+    }.store(in: &cancellables)
+  }
+  
+  func testResultWithCustomTipFor4Person() {
+    // given
+    let bill: Double = 200.0
+    let tip: Tip = .custom(value: 201)
+    let split: Int = 4
+    let input = buildInput(
+      bill: bill,
+      tip: tip,
+      split: split)
+    // when
+    let output = sut.transform(input: input)
+    // then
+    output.updateViewPublisher.sink { result in
+      XCTAssertEqual(result.amountPerPerson, 100.25)
+      XCTAssertEqual(result.totalBill, 401)
+      XCTAssertEqual(result.totalTip, 201)
+    }.store(in: &cancellables)
+  }
+  
+//  - testResultWithoutTipFor2Person
+//  - testResultWith10PercentTipFor2Person
+//  - testResultWithCustomTipFor4Person
+  
   private func buildInput(bill: Double, tip: Tip, split: Int) -> CalculatorVM.Input {
     return .init(
       billPublisher: Just(bill).eraseToAnyPublisher(),
